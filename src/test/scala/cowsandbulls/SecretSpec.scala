d3e07fa274e865a3f0dc72c8c8cd6cfc4186bda9
kuangyi <kuangyi.xing@rea-group.com>
2019-01-03 14:12:30 +1100

    gen and property based testing

--- src/test/scala/cowsandbulls/SecretSpec.scala
+++ src/test/scala/cowsandbulls/SecretSpec.scala
@@ -1,13 +1,77 @@
 package cowsandbulls
 
+import org.scalacheck.{Arbitrary, Gen}
+import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 
 import scala.collection.mutable
+import scala.util.Random
 
-class SecretSpec extends Specification {
+class SecretSpec extends Specification with ScalaCheck {
 
-  //TODO: Property testing
+  case class ZZZ(x: String)
+
+  val GenSecret: Gen[Secret] = for {
+    x1 <- Gen.choose(0, 9)
+    x2 <- Gen.choose(0, 9)
+    x3 <- Gen.choose(0, 9)
+    x4 <- Gen.choose(0, 9)
+  } yield Secret(x1,x2,x3,x4)
+
+  case class SecretPair(s1: Secret, s2: Secret)
+
+  val GenNonOverlappingPair: Gen[SecretPair] = {
+    for {
+      s1: Secret <- GenSecret
+      s2: Secret <- GenSecret.suchThat(s => s.value.intersect(s1.value).isEmpty)
+    } yield SecretPair(s1, s2)
+  }
+
+  implicit val arbGenPair: Arbitrary[SecretPair] = Arbitrary(GenNonOverlappingPair)
+
+  implicit val arbSecret: Arbitrary[Secret] = Arbitrary(GenSecret)
+//  private implicit val arbSecret: Gen[Secret] = foo.map(s => Secret(Vector(s,s,s,s))
+
   "Secret#evaluateGuess" should {
+    "when nothing in common, it returns 0 bull and 0 cow" in {
+      prop { (pair: SecretPair) =>
+        val result = pair.s1.evaluateGuess(pair.s2)
+        result must beEqualTo(Result(0, 0))
+      }
+    }
+
+      "total number of cows and bulls should be between 0 and 4" in {
+      prop { (secret: Secret, guess: Secret) =>
+        val result = secret.evaluateGuess(guess)
+        val total: Int = result.bullNum + result.cowNum
+        total must beBetween(0, 4)
+      }
+    }
+
+    "swapping secret and guess should give us the same result" in {
+      prop { (secret: Secret, guess: Secret) =>
+        val result = secret.evaluateGuess(guess)
+        val reversedResult = guess.evaluateGuess(secret)
+        result must beEqualTo(reversedResult)
+      }
+    }
+
+    "total matches will be independent of ordering" in {
+      prop { (secret: Secret, guess: Secret) =>
+        val shuffledGuess = Secret(Random.shuffle(guess.value))
+        val result = secret.evaluateGuess(guess)
+        val shuffledResult = secret.evaluateGuess(shuffledGuess)
+        result.cowNum + result.bullNum must beEqualTo(shuffledResult.bullNum + shuffledResult.cowNum)
+      }
+    }
+
+    "Evaluating self value should return 4 bulls" in {
+      prop { (secret: Secret) =>
+        val result = secret.evaluateGuess(secret)
+        result must beEqualTo(Result(cowNum = 0, bullNum = 4))
+      }
+    }
+
     "when nothing in common, it returns 0 bull and 0 cow" in {
       val secret = Secret(1, 2, 3, 4)
       val guess = Secret(5, 6, 7, 8)
