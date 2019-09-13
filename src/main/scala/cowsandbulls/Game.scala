023098cf6aec9a18d9c5d7787bd1d71419eb3c05
kuangyi <kuangyi.xing@rea-group.com>
2019-09-13 14:49:15 +1000

    promp before typing an input

--- src/main/scala/cowsandbulls/Game.scala
+++ src/main/scala/cowsandbulls/Game.scala
@@ -6,7 +6,7 @@ import cats.implicits._
 
 import scala.io.StdIn
 
-case class Game[F[_]: Monad](
+case class Game[F[_] : Monad](
   secret: Secret,
   inputLine: F[Option[String]],
   output: (String) => F[Unit]
@@ -14,18 +14,23 @@ case class Game[F[_]: Monad](
 
   private val InputGuess: F[Option[Guess]] = inputLine.map(_.map(input => Secret(input)))
 
-  def playOneTurn(x: Unit): F[Either[Unit, Unit]] = InputGuess.flatMap {
-    case Some(guess) => {
-      val result: Result = secret.evaluateGuess(guess)
-      if (result.bullNum == 4) {
-        output("You win").map(_ => Right(()))
-      } else {
-        output(s"Cows: ${result.cowNum}, Bulls: ${result.bullNum}")
-          .map(_ => Left(()))
-      }
-    }
+  private val Continue = Left(())
+  private val Terminate = Right(())
 
-    case None => Monad[F].pure(Right(()))
+  def playOneTurn(x: Unit): F[Either[Unit, Unit]] = {
+    output("Input a guess") *>
+      InputGuess.flatMap {
+        case Some(guess) => {
+          val result: Result = secret.evaluateGuess(guess)
+          if (result.bullNum == 4) {
+            output("You win").map(_ => Terminate)
+          } else {
+            output(s"Cows: ${result.cowNum}, Bulls: ${result.bullNum}")
+              .map(_ => Continue)
+          }
+        }
+        case None => Monad[F].pure(Right(()))
+      }
   }
 
   def play(): F[Unit] = {
@@ -38,8 +43,12 @@ object Game {
   def main(args: Array[String]): Unit = {
     val game: Game[IO] = Game(
       Secret.random(),
-      IO { Option.apply(StdIn.readLine())},
-      s => IO {println(s)}
+      IO {
+        Option.apply(StdIn.readLine())
+      },
+      s => IO {
+        println(s)
+      }
     )
 
     game.play().unsafeRunSync()
